00d98ee0d6c763d7d1194798d03814f00dbe7763
evilsmile <taili.shih@gmail.com>
2021-07-26 00:13:33 +0800

    fix: README.md and wording

--- example_pool_test.go
+++ example_pool_test.go
@@ -16,7 +16,7 @@ func ExampleNewPool_withFixedSize() {
 
 func ExampleNewPool_withIncreasingSize() {
 	// allocate a pool with maximum size 5, and initialize 2 goroutines.
-	// if necessary, the number of goroutines increases to 5 and never go down.
+	// if necessary, the number of goroutines increase to 5 and never go down.
 	p := goroutines.NewPool(5, goroutines.WithPreAllocWorkers(2))
 	// don't forget to release the resource in the end
 	defer p.Release()
@@ -24,7 +24,7 @@ func ExampleNewPool_withIncreasingSize() {
 
 func ExampleNewPool_withAutoScaledSize() {
 	// allocate a pool with maximum size 5, and initialize 2 goroutines.
-	// if necessary, the number of goroutines increases to 5.
+	// if necessary, the number of goroutines increase to 5.
 	// if not busy ( by checking the running status every 10 seconds ), the number goes to 2.
 	p := goroutines.NewPool(
 		5,
@@ -75,4 +75,45 @@ func ExamplePool_Schedule() {
 	// index: 5
 	// index: 6
 	// index: 0
+}
+
+func ExamplePool_ScheduleWithTimeout() {
+	totalN, taskN := 5, 5
+	pause := make(chan struct{})
+	rets := make(chan int, taskN)
+
+	// allocate a pool with 5 goroutines to deal with those 5 tasks
+	p := goroutines.NewPool(totalN)
+	// don't forget to release the pool in the end
+	defer p.Release()
+
+	// full the workers which are stopped with the `pause`
+	for i := 0; i < taskN; i++ {
+		idx := i
+		p.ScheduleWithTimeout(50*time.Millisecond, func() {
+			<-pause
+			rets <- idx
+		})
+	}
+
+	// no more chance to add any task in Pool, and return `ErrScheduleTimeout`
+	if err := p.ScheduleWithTimeout(50*time.Millisecond, func() {
+		<-pause
+		rets <- taskN
+	}); err != nil {
+		fmt.Println(err.Error())
+	}
+
+	close(pause)
+	for i := 0; i < taskN; i++ {
+		fmt.Println("index:", <-rets)
+	}
+
+	// Unordered output:
+	// schedule timeout
+	// index: 0
+	// index: 3
+	// index: 2
+	// index: 4
+	// index: 1
 }
